a05355ae82a532bf864a5c1b33cf6281b563c1c1
aruljenshiya <jenshiya1996@gmail.com>
2016-11-03 09:18:05 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,25 @@
-console.log('Loaded!');
-//Move the image
-var img = document.getElementById('modi');
-var marginLeft=0;
-function moveRight () {
-    marginLeft = marginLeft +1;
-    img.style.marginLeft= marginLeft+'px';
-}
-img.onclick = function () {
-    var interval =setInterval(moveRight,50);
+//counter code
+var button=document.getElementById('counter');
+
+
+button.onclick =function () {
     
-    
+  // Create a request object
+  var request=XMLHttpRequest();
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function(){
+      if(request.readyState=== XMLHttpRequest.DONE) {
+          // Take some action
+          if(request.status===200) {
+             var counter= request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+      }
+      //Not done yet
+  };
+ //Make a request
+ request.open('GET','http://aruljenshiya.imad.hasura-app.io/counter',true);
+ request.send(null);
+ 
 };
