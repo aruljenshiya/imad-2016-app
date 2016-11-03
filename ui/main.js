ba18b29513a832d22ddfabf2abab80fb2bc6f83a
aruljenshiya <jenshiya1996@gmail.com>
2016-11-03 08:04:57 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
 console.log('Loaded!');
 //Move the image
 var img = document.getElementById('modi');
+var marginLeft=0;
+function moveRight () {
+    marginLeft = marginLeft +10;
+    img.style.marginLeft= marginLeft+'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval =setInterval(moveRight,100);
+    
     
 };
